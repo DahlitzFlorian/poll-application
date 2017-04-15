f28bdf194ce287d9e2c203f20d9e4cb022c9ea81
florian dahlitz <f2dahlitz@freenet.de>
2017-04-15 21:23:43 +0200

    added three more views

--- poll/urls.py
+++ poll/urls.py
@@ -3,7 +3,19 @@ from . import views
 
 
 urlpatterns = [
-    # the row below shows the default opened file if nothing else
-    # was specified by the url -> in this case index (in views.py)
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+
+    # parentheses 'capture' the text of the url in it and send it
+    # as argument to the method
+    # ?P<question_id> ends up as question_id="" and the [0-9]+
+    # stands for digits that have to stand there that it matches
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -6,3 +6,19 @@ from django.http import HttpResponse
 # specified by the url
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+# view for details
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+# view results
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+# view vote
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
