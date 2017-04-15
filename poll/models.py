5fe43a86a1a2e0000630a234395d1b1d15c09896
florian dahlitz <f2dahlitz@freenet.de>
2017-04-15 17:18:04 +0200

    migrate Question and Choice of poll

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# Question-Class managing question texts and its publication date
+class Question(models.Model):
+    # variables beneath represent attributes of the objects
+    # as well as columns in the db tables
+    question_text = models.CharField(max_length=200)
+
+    # 'date published' as first argument is the human-readable
+    # name. If this argument is not given, Django uses the
+    # machine-readable name (simply the name of the variable)
+    pub_date = models.DateTimeField('date published')
+
+
+# Choice-Class managing to a question the choice text and the
+# number of votes
+class Choice(models.Model):
+    # ForeignKey defines a relationship between each Choice-object
+    # and a single question.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
