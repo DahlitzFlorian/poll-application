0e06c2b867edd9d6f38c68de2a96110a84cb06c5
florian dahlitz <f2dahlitz@freenet.de>
2017-04-16 11:52:52 +0200

    fixed method bug

--- poll/models.py
+++ poll/models.py
@@ -23,7 +23,8 @@ class Question(models.Model):
 
     # defining a simple method
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 # Choice-Class managing to a question the choice text and the
