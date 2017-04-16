7d5596588fcc9919db104ec0abf8ba4f57ac7ab6
florian dahlitz <f2dahlitz@freenet.de>
2017-04-16 18:20:03 +0200

    added view tests

--- poll/views.py
+++ poll/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question
 
@@ -17,8 +18,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (Not including those 
+        set to be published in the future)
+        """
+        # the filter says that it only returns those w/ the pub_date
+        # less or equal to timezone.now() (earlier or now)
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -27,6 +35,12 @@ class DetailView(generic.DetailView):
     # DetailView uses by default the following template_name if not specified
     # in templates directory <app_name>/<model_name>_detail.html
     template_name = 'poll/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
