a9b61ddb0512d40cd3c39050383a7820bb69b447
florian dahlitz <f2dahlitz@freenet.de>
2017-04-17 19:26:51 +0200

    changed admin panel structure

--- poll/admin.py
+++ poll/admin.py
@@ -2,5 +2,31 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# class providing choice related to question
+# class ChoiceInline(admin.StackedInline) -> stacked form
+# the following provides it as table
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+# class to customize question model in admin panel
+class QuestionAdmin(admin.ModelAdmin):
+    # classify the add and change pages into fieldsets
+    fieldsets = [
+        (None, {'fields' : ['question_text']}),
+        ('Date information', {'fields' : ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+    # customizes the shown table on the 'show all'/'changes'-page
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    # add list filters at the sidebar
+    list_filter = ['pub_date']
+
+    # add search capabilities
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- poll/models.py
+++ poll/models.py
@@ -25,6 +25,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    # customize was_published_recently() output in admin panel
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 # Choice-Class managing to a question the choice text and the
